fbe10d1c110bf706c90a5935537a644081e1b0ee
evo20537-stack <evo20537@gmail.com>
2025-12-21 14:52:59 +0800

    Update Card.tsx

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -13,14 +13,11 @@ const Card: React.FC<CardProps> = ({ children, className = '', onClick, noPaddin
       onClick={onClick}
       className={`
         bg-white rounded-2xl border-2 border-stone-100
-        transition-all duration-200
+        transition-all duration-200 cozy-shadow
         ${onClick ? 'active:scale-[0.98] cursor-pointer' : ''}
         ${noPadding ? '' : 'p-4'}
         ${className}
       `}
-      style={{
-        boxShadow: '4px 4px 0px #E0E5D5' // The requested soft shadow
-      }}
     >
       {children}
     </div>
